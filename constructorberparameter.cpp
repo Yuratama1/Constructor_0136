b5ec6ec3cc8c582cba46f930cc4550cb00b8c763
Yuratama1 <yuratama661@gmail.com>
2025-05-13 18:51:53 +0700

    memebuat constructor berparameter

---
+++ constructorberparameter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+    public:
+        //constructor dengan parameter
+        mahasiswa(int nim, string nama);
+};
+
