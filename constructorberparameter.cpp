24276f56a8d5ad4169762e28beb1052acc89927c
Yuratama1 <yuratama661@gmail.com>
2025-05-13 18:57:17 +0700

    membuat int main

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -12,5 +12,10 @@ mahasiswa::mahasiswa(int nim, string nama){
     cout << "Constructor Dengan Parameter Terpanggil" << endl;
     cout << "NIM : " << nim << endl;
     cout << "Nama : " << nama << endl;
+}
 
+int main(){
+    //memanggil constructor dgn parameter
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
 }
