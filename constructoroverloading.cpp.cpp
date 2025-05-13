026a60778f88260788be2256703dfc87a1bc111e
Yuratama1 <yuratama661@gmail.com>
2025-05-13 19:09:44 +0700

    memebuat prosedur constraint

--- constructoroverloading.cpp.cpp
+++ constructoroverloading.cpp.cpp
@@ -14,3 +14,16 @@ class mahasiswa{
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+mahasiswa::mahasiswa(){
+}
+mahasiswa::mahasiswa(int iNim){
+    nim = iNim; //definisi Hanya NIM
+}
+mahasiswa::mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+mahasiswa::mahasiswa(int iNim, string iNama){
+     nim = iNim;
+     nama = iNama;
+}
