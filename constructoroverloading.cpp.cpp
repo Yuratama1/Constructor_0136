9f921138872eb51d2623b69670b56ac9e2a0554e
Yuratama1 <yuratama661@gmail.com>
2025-05-13 19:15:41 +0700

    memebuat int main

--- constructoroverloading.cpp.cpp
+++ constructoroverloading.cpp.cpp
@@ -32,3 +32,16 @@ void mahasiswa::cetak(){
     cout << endl << "Nim = " <<nim << endl;
     cout << "Nama = " << nama << endl;
 }
+
+int main(){
+    mahasiswa mhs1(102030); // Nim
+    mahasiswa mhs2("Yura"); // Nama
+    mahasiswa mhs3(010203, "Santi"); // Nim dan Nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
